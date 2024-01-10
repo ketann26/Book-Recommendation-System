f7d4a9088f65b86f8d9788b0c7b72eea8fd3499c
ketann26 <ketan9sharma@yahoo.in>
2024-01-11 02:26:55 +0530

    Front End Logic Updates

--- app.py
+++ app.py
@@ -4,26 +4,31 @@ import numpy as np
 
 model = pickle.load(open("model.pkl",'rb'))
 book_pivot = pickle.load(open("book_pivot.pkl",'rb'))
+title_list = book_pivot.index.to_list()
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title_list=title_list)
 
 @app.route('/recommend', methods=['POST'])
 def recommend():
     title = request.form.get('title')
+    print(title)
+    if(title==None):
+        return render_template("index.html",title_list=title_list)
 
     # recommendation
     res = []
     book_id = np.where(book_pivot.index == title)[0][0]
     distances, suggestions = model.kneighbors(book_pivot.iloc[book_id].values.reshape(1,-1), n_neighbors=6)
-    print(suggestions)
     for i in range(len(suggestions[0])):
         if i!=0:
             res.append(book_pivot.index[suggestions[0][i]])
 
-    return render_template("index.html", res=res)
+    return render_template("index.html", res=res, title_list=title_list, book_title=title)
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0',port=8080)
+    # app.run(host='0.0.0.0',port=8080)
+    app.run(debug=True)
